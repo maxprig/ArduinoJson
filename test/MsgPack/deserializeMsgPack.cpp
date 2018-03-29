7302e25bc22f5feded679f01067ded35c58d9f07
Benoit Blanchon <github@benoitblanchon.fr>
2018-03-29 09:03:53 +0200

    Added helper function to remove duplication

--- test/MsgPack/deserializeMsgPack.cpp
+++ test/MsgPack/deserializeMsgPack.cpp
@@ -5,38 +5,34 @@
 #include <ArduinoJson.h>
 #include <catch.hpp>
 
+template <typename Integer>
+void check(uint8_t input[], Integer expected) {
+  DynamicJsonVariant variant;
+
+  bool success = deserializeMsgPack(variant, input);
+
+  REQUIRE(success == true);
+  REQUIRE(variant.is<Integer>());
+  REQUIRE(variant.as<Integer>() == expected);
+}
+
 TEST_CASE("deserializeMsgPack(JsonVariant&)") {
   DynamicJsonVariant variant;
 
   SECTION("nil format") {
     uint8_t input[] = {0xc0};
-
-    bool success = deserializeMsgPack(variant, input);
-
-    REQUIRE(success == true);
-    REQUIRE(variant.is<char*>());
-    REQUIRE(variant.as<char*>() == static_cast<const char*>(0));
+    check<const char*>(input, 0);
   }
 
   SECTION("bool format family") {
     SECTION("false") {
       uint8_t input[] = {0xc2};
-
-      bool success = deserializeMsgPack(variant, input);
-
-      REQUIRE(success == true);
-      REQUIRE(variant.is<bool>());
-      REQUIRE(variant.as<bool>() == false);
+      check(input, false);
     }
 
     SECTION("true") {
       uint8_t input[] = {0xc3};
-
-      bool success = deserializeMsgPack(variant, input);
-
-      REQUIRE(success == true);
-      REQUIRE(variant.is<bool>());
-      REQUIRE(variant.as<bool>() == true);
+      check(input, true);
     }
   }
 
@@ -44,130 +40,70 @@ TEST_CASE("deserializeMsgPack(JsonVariant&)") {
     SECTION("7-bit positive integer") {
       SECTION("0") {
         uint8_t input[] = {0x00};
-
-        bool success = deserializeMsgPack(variant, input);
-
-        REQUIRE(success == true);
-        REQUIRE(variant.is<int>());
-        REQUIRE(variant.as<int>() == 0);
+        check(input, 0);
       }
 
       SECTION("127") {
         uint8_t input[] = {0x7F};
-
-        bool success = deserializeMsgPack(variant, input);
-
-        REQUIRE(success == true);
-        REQUIRE(variant.is<int>());
-        REQUIRE(variant.as<int>() == 127);
+        check(input, 127);
       }
     }
 
     SECTION("5-bit negative integer") {
       SECTION("-32") {
         uint8_t input[] = {0xe0};
-
-        bool success = deserializeMsgPack(variant, input);
-
-        REQUIRE(success == true);
-        REQUIRE(variant.is<int>());
-        REQUIRE(variant.as<int>() == -32);
+        check(input, -32);
       }
 
       SECTION("-1") {
         uint8_t input[] = {0xff};
-
-        bool success = deserializeMsgPack(variant, input);
-
-        REQUIRE(success == true);
-        REQUIRE(variant.is<int>());
-        REQUIRE(variant.as<int>() == -1);
+        check(input, -1);
       }
     }
 
     SECTION("8-bit unsigned integer") {
       SECTION("0") {
         uint8_t input[] = {0xcc, 0x00};
-
-        bool success = deserializeMsgPack(variant, input);
-
-        REQUIRE(success == true);
-        REQUIRE(variant.is<int>());
-        REQUIRE(variant.as<int>() == 0);
+        check(input, 0);
       }
 
       SECTION("255") {
         uint8_t input[] = {0xcc, 0xff};
-
-        bool success = deserializeMsgPack(variant, input);
-
-        REQUIRE(success == true);
-        REQUIRE(variant.is<int>());
-        REQUIRE(variant.as<int>() == 255);
+        check(input, 255);
       }
     }
 
     SECTION("16-bit big-endian unsigned integer") {
       SECTION("0") {
         uint8_t input[] = {0xcd, 0x00, 0x00};
-
-        bool success = deserializeMsgPack(variant, input);
-
-        REQUIRE(success == true);
-        REQUIRE(variant.is<int>());
-        REQUIRE(variant.as<int>() == 0);
+        check(input, 0);
       }
 
       SECTION("65535") {
         uint8_t input[] = {0xcd, 0xFF, 0xFF};
-
-        bool success = deserializeMsgPack(variant, input);
-
-        REQUIRE(success == true);
-        REQUIRE(variant.is<int>());
-        REQUIRE(variant.as<int>() == 65535);
+        check(input, 65535);
       }
 
       SECTION("12345") {
         uint8_t input[] = {0xcd, 0x30, 0x39};
-
-        bool success = deserializeMsgPack(variant, input);
-
-        REQUIRE(success == true);
-        REQUIRE(variant.is<int>());
-        REQUIRE(variant.as<int>() == 12345);
+        check(input, 12345);
       }
     }
 
     SECTION("32-bit big-endian unsigned integer") {
       SECTION("0x00000000") {
         uint8_t input[] = {0xce, 0x00, 0x00, 0x00, 0x00};
-
-        bool success = deserializeMsgPack(variant, input);
-
-        REQUIRE(success == true);
-        REQUIRE(variant.is<uint32_t>());
-        REQUIRE(variant.as<uint32_t>() == 0x00000000);
+        check(input, 0x00000000);
       }
 
       SECTION("0xFFFFFFFF") {
         uint8_t input[] = {0xce, 0xFF, 0xFF, 0xFF, 0xFF};
-
-        bool success = deserializeMsgPack(variant, input);
-
-        REQUIRE(success == true);
-        REQUIRE(variant.is<uint32_t>());
-        REQUIRE(variant.as<uint32_t>() == 0xFFFFFFFF);
+        check(input, 0xFFFFFFFF);
       }
 
       SECTION("0x12345678") {
         uint8_t input[] = {0xce, 0x12, 0x34, 0x56, 0x78};
-
-        bool success = deserializeMsgPack(variant, input);
-
-        REQUIRE(success == true);
-        REQUIRE(variant.is<uint32_t>());
-        REQUIRE(variant.as<uint32_t>() == 0x12345678);
+        check(input, 0x12345678);
       }
     }
 
@@ -175,34 +111,20 @@ TEST_CASE("deserializeMsgPack(JsonVariant&)") {
       SECTION("0x0000000000000000") {
         uint8_t input[] = {0xcf, 0x00, 0x00, 0x00, 0x00,
                            0x00, 0x00, 0x00, 0x00};
-
-        bool success = deserializeMsgPack(variant, input);
-
-        REQUIRE(success == true);
-
 #if ARDUINOJSON_USE_LONG_LONG || ARDUINOJSON_USE_INT64
-        REQUIRE(variant.is<uint64_t>());
-        REQUIRE(variant.as<uint64_t>() == 0);
+        check<uint64_t>(input, 0);
 #else
-        REQUIRE(variant.is<uint32_t>());
-        REQUIRE(variant.as<uint32_t>() == 0);
+        check<uint32_t>(input, 0);
 #endif
       }
 
       SECTION("0xFFFFFFFFFFFFFFFF") {
         uint8_t input[] = {0xcf, 0xFF, 0xFF, 0xFF, 0xFF,
                            0xFF, 0xFF, 0xFF, 0xFF};
-
-        bool success = deserializeMsgPack(variant, input);
-
-        REQUIRE(success == true);
-
 #if ARDUINOJSON_USE_LONG_LONG || ARDUINOJSON_USE_INT64
-        REQUIRE(variant.is<uint64_t>());
-        REQUIRE(variant.as<uint64_t>() == 0xFFFFFFFFFFFFFFFF);
+        check<uint64_t>(input, 0xFFFFFFFFFFFFFFFF);
 #else
-        REQUIRE(variant.is<uint32_t>());
-        REQUIRE(variant.as<uint32_t>() == 0xFFFFFFFF);
+        check<uint32_t>(input, 0xFFFFFFFF);
 #endif
       }
 
@@ -210,16 +132,10 @@ TEST_CASE("deserializeMsgPack(JsonVariant&)") {
         uint8_t input[] = {0xcf, 0x12, 0x34, 0x56, 0x78,
                            0x9A, 0xBC, 0xDE, 0xF0};
 
-        bool success = deserializeMsgPack(variant, input);
-
-        REQUIRE(success == true);
-
 #if ARDUINOJSON_USE_LONG_LONG || ARDUINOJSON_USE_INT64
-        REQUIRE(variant.is<uint64_t>());
-        REQUIRE(variant.as<uint64_t>() == 0x123456789ABCDEF0);
+        check<uint64_t>(input, 0x123456789ABCDEF0);
 #else
-        REQUIRE(variant.is<uint32_t>());
-        REQUIRE(variant.as<uint32_t>() == 0x9ABCDEF0);
+        check<uint32_t>(input, 0x9ABCDEF0);
 #endif
       }
     }
@@ -227,54 +143,29 @@ TEST_CASE("deserializeMsgPack(JsonVariant&)") {
     SECTION("8-bit signed integer") {
       SECTION("0") {
         uint8_t input[] = {0xd0, 0x00};
-
-        bool success = deserializeMsgPack(variant, input);
-
-        REQUIRE(success == true);
-        REQUIRE(variant.is<int>());
-        REQUIRE(variant.as<int>() == 0);
+        check(input, 0);
       }
 
       SECTION("-1") {
         uint8_t input[] = {0xd0, 0xff};
-
-        bool success = deserializeMsgPack(variant, input);
-
-        REQUIRE(success == true);
-        REQUIRE(variant.is<int>());
-        REQUIRE(variant.as<int>() == -1);
+        check(input, -1);
       }
     }
 
     SECTION("16-bit big-endian signed integer") {
       SECTION("0") {
         uint8_t input[] = {0xd1, 0x00, 0x00};
-
-        bool success = deserializeMsgPack(variant, input);
-
-        REQUIRE(success == true);
-        REQUIRE(variant.is<int>());
-        REQUIRE(variant.as<int>() == 0);
+        check(input, 0);
       }
 
       SECTION("-1") {
         uint8_t input[] = {0xd1, 0xFF, 0xFF};
-
-        bool success = deserializeMsgPack(variant, input);
-
-        REQUIRE(success == true);
-        REQUIRE(variant.is<int>());
-        REQUIRE(variant.as<int>() == -1);
+        check(input, -1);
       }
 
       SECTION("-12345") {
         uint8_t input[] = {0xd1, 0xcf, 0xc7};
-
-        bool success = deserializeMsgPack(variant, input);
-
-        REQUIRE(success == true);
-        REQUIRE(variant.is<int>());
-        REQUIRE(variant.as<int>() == -12345);
+        check(input, -12345);
       }
     }
   }
