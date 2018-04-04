996b7d78c43fe087e63996d6b4bf19f0764fd90e
Benoit Blanchon <github@benoitblanchon.fr>
2018-04-04 10:07:27 +0200

    Restore tests

--- src/ArduinoJson/MsgPack/ieee754.hpp
+++ src/ArduinoJson/MsgPack/ieee754.hpp
@@ -7,7 +7,7 @@
 namespace ArduinoJson {
 namespace Internals {
 
-inline void doubleToFloat(uint8_t d[8], uint8_t f[4]) {
+inline void doubleToFloat(const uint8_t d[8], uint8_t f[4]) {
   f[0] = uint8_t((d[0] & 0xC0) | (d[0] << 3 & 0x3f) | (d[1] >> 5));
   f[1] = uint8_t((d[1] << 3) | (d[2] >> 5));
   f[2] = uint8_t((d[2] << 3) | (d[3] >> 5));
--- test/MsgPack/readDouble.cpp
+++ test/MsgPack/readDouble.cpp
@@ -9,11 +9,15 @@ using namespace ArduinoJson::Internals;
 
 template <typename T>
 static void check(const char* input, T expected) {
-  const uint8_t* bytes = reinterpret_cast<const uint8_t*>(input);
-  REQUIRE(readDouble<T>(bytes) == expected);
+  T actual;
+  uint8_t* f = reinterpret_cast<uint8_t*>(&actual);
+  const uint8_t* d = reinterpret_cast<const uint8_t*>(input);
+  doubleToFloat(d, f);
+  fixEndianess(actual);
+  CHECK(actual == expected);
 }
 
-TEST_CASE("Internals::readDouble()") {
+TEST_CASE("Internals::doubleToFloat()") {
   check("\x40\x09\x21\xCA\xC0\x83\x12\x6F", 3.1415f);
   check("\x00\x00\x00\x00\x00\x00\x00\x00", 0.0f);
   check("\x80\x00\x00\x00\x00\x00\x00\x00", -0.0f);
